f1bfe71c034d07d1ae1615c698fa3b117a5540f3
TRINITY003 <lawaloluwatobi404@gmail.com>
2022-06-23 01:13:51 +0100

    initial commit

---
+++ Untitled-1.cs
@@ -0,0 +1,12 @@
+ Console.WriteLine("Enter your first name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your favorite colour");
+            string favouritecolour = Console.ReadLine();
+            Console.WriteLine("Enter your favorite food");
+            string favoritefood = Console.ReadLine();
+            Console.WriteLine("Enter your state of origin");
+            string state of origin = Console.ReadLine
